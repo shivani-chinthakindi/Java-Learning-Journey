2f03071e8eb0c263083cd7d70728c46bb3d28486
Shivani Patel <schinthakindishivani@gmail.com>
2026-06-06 17:45:24 +0530

    Added Variables and Data Types Examples

--- 01-Basics/VariablesAndDataTypes.java
+++ 01-Basics/VariablesAndDataTypes.java
@@ -1,1 +1,34 @@
+public class VariablesAndDataTypes {
+    public static void main(String[] args){
 
+        // VARIABLES
+        int x = 3;
+        int y = 4;
+        // Here x and y are the variable names, 3 and 4 are the values and int is the type of the variable i.e., data type
+        System.out.println("The sum of the variables is "+ (x + y));
+
+        // DATATYPES
+
+        byte b=9;
+        short s=44;
+        int i=899;
+        long l=33445556666689L;
+        float f=4.3f;
+        double d=5.43532635322;
+        char c='A';
+        boolean boo=true;
+        String str="Shivani";
+        int[] nums={1,2,3,4,5};
+    System.out.println(b);
+    System.out.println(s);
+    System.out.println(i);
+    System.out.println(l);
+    System.out.println(f);
+    System.out.println(d);
+    System.out.println(c);
+    System.out.println(boo);
+    System.out.println(str);
+    System.out.println(nums[2]);
+
+    }
+}
