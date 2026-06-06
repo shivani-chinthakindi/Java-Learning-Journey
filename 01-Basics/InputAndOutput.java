6846b85fc3e58153d1d8674a38038b85d2fd1a4a
Shivani Patel <schinthakindishivani@gmail.com>
2026-06-06 18:03:57 +0530

    Added input and output examples

--- 01-Basics/InputAndOutput.java
+++ 01-Basics/InputAndOutput.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
 
+public class InputAndOutput {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("=== Input and Output Demo ===");
+
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+
+        System.out.println("\n=== Output ===");
+        System.out.println("Hello " + name + "!");
+        System.out.println("You are " + age + " years old.");
+
+        sc.close();
+    }
+}
