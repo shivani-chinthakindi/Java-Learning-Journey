a808a298ba20c0f81e6494d6f6476f6989ab327f
Shivani Patel <schinthakindishivani@gmail.com>
2026-06-06 18:16:42 +0530

    Completed loops in java

--- 01-Basics/Loops.java
+++ 01-Basics/Loops.java
@@ -1,1 +1,28 @@
+public class Loops {
+    public static void main(String[] args){
+        System.out.println("----for loop----");
+        for(int i = 0; i <= 5; i++){
+            System.out.println("i = "+i);
+        }
 
+        System.out.println("----while loop----");
+        int j = 1;
+        while(j <= 5){
+            System.out.println("j = "+j);
+            j++;
+        }
+
+        System.out.println("----do-while loop----");
+        int k = 1;
+        do{
+            System.out.println("k = "+k);
+            k++;
+        }while(k <= 5);
+
+        System.out.println("----enhanced for loop----");
+        int[] numbers = {10, 20, 30, 40, 50};
+        for(int num:numbers){
+            System.out.println("num = "+num);
+        }
+    }
+}
