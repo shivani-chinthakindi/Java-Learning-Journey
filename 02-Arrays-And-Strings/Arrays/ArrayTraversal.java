3a8906023e7e3796a37f04bab91f32eec450e691
Shivani Patel <schinthakindishivani@gmail.com>
2026-06-18 18:55:14 +0530

    Complete array traversal

--- 02-Arrays-And-Strings/Arrays/ArrayTraversal.java
+++ 02-Arrays-And-Strings/Arrays/ArrayTraversal.java
@@ -1,1 +1,10 @@
+class ArrayTraversal {
+    public static void main (String[] args) {
 
+        String[] chocolates = {"dairy milk", "5 Star", "kitkat", "munch", "snickers"};
+
+        for (String item: chocolates) {
+            System.out.println(item);
+        }
+    }
+}
