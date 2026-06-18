83bfab273f04eab2f416514a3febb3a92652f6ac
Shivani Patel <schinthakindishivani@gmail.com>
2026-06-18 18:44:24 +0530

    Complete array input and output

--- 02-Arrays-And-Strings/Arrays/ArrayInputOutput.java
+++ 02-Arrays-And-Strings/Arrays/ArrayInputOutput.java
@@ -1,1 +1,16 @@
+class ArrayInputOutput {
+    public static void main (String[] args) {
 
+        int[] nums = new int[5];
+
+        nums[0] = 11;
+        nums[1] = 22;
+        nums[2] = 33;
+        nums[3] = 44;
+        nums[4] = 55;
+        
+        for (int num: nums) {
+            System.out.print(num + " ");
+        }
+    }
+}
