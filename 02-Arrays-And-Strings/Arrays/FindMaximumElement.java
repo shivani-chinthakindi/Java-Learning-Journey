a88999a511d2a4597a4e4b8872eb42b6bedbcbf2
Shivani Patel <schinthakindishivani@gmail.com>
2026-06-19 22:17:28 +0530

    Complete finding maximum element

--- 02-Arrays-And-Strings/Arrays/FindMaximumElement.java
+++ 02-Arrays-And-Strings/Arrays/FindMaximumElement.java
@@ -1,1 +1,16 @@
+public class FindMaximumElement {
+    public static void main (String[] args) {
+        int[] arr = {22,58,29,19,39,44};
+        System.out.println(findMax(arr));
+    }
 
+    static int findMax (int[] arr){
+        int max = arr[0];
+        for (int i = 1; i < arr.length-1; i++) {
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+}
