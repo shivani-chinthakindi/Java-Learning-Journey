8275bf25ea5c054181d077c58bb53c294bd50413
Shivani Patel <schinthakindishivani@gmail.com>
2026-06-19 22:44:21 +0530

    Add multi dimensional array

--- 02-Arrays-And-Strings/Arrays/FindMinimumElement.java
+++ 02-Arrays-And-Strings/Arrays/FindMinimumElement.java
@@ -1,1 +1,16 @@
+public class FindMinimumElement {
+    public static void main (String[] args) {
+        int[] arr = {22,58,29,19,39,44};
+        System.out.println(findMin(arr));
+    }
 
+    static int findMin (int[] arr){
+        int min = arr[0];
+        for (int i = 1; i < arr.length-1; i++) {
+            if(arr[i] < min){
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+}
--- 02-Arrays-And-Strings/Arrays/MultiDimensionalArray.java
+++ 02-Arrays-And-Strings/Arrays/MultiDimensionalArray.java
@@ -1,1 +1,25 @@
-
+import java.util.*;
+public class MultiDimensionalArray {
+    public static void main (String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter number of rows: ");
+        int r = in.nextInt();
+        System.out.println("Enter number of columns: ");
+        int c = in.nextInt();
+        int[][] arr = new int[r][c];
+        System.out.println("Enter elements of array: ");
+        for(int i = 0; i < r; i++){
+            for(int j = 0; j < c; j++){
+                arr[i][j] = in.nextInt();
+            }
+        }
+        System.out.println("Elements of array are: ");
+        for(int i = 0; i < r; i++){
+            for(int j = 0; j < c; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+}
