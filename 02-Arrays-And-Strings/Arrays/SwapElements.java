e39cb22a7cc927e9a4c051c5ef37e31a97a638ec
Shivani Patel <schinthakindishivani@gmail.com>
2026-06-20 23:25:53 +0530

    Add swap elements

--- 02-Arrays-And-Strings/Arrays/SwapElements.java
+++ 02-Arrays-And-Strings/Arrays/SwapElements.java
@@ -1,1 +1,13 @@
-
+import java.util.Arrays;
+public class SwapElements {
+    public static void main(String[] args){
+        int[] arr = {12, 45, 63, 68, 24};
+        swap(arr,0,3);
+        System.out.print(Arrays.toString(arr));
+    }
+    static void swap(int[] arr, int index1, int index2){
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+}
