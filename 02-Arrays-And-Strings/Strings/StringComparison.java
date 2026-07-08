90aafea5594d5018bfd682b066e826d9b2b03ec1
Shivani Patel <schinthakindishivani@gmail.com>
2026-07-08 19:34:45 +0530

    Add string comparison

--- 02-Arrays-And-Strings/Strings/StringComparison.java
+++ 02-Arrays-And-Strings/Strings/StringComparison.java
@@ -1,1 +1,11 @@
-
+class StringComparison{
+    public static void main(String[] args) {
+        String a = "Shivani";
+        String b = "Shivani";
+        System.out.println(a == b); // checks both the value and to which it is pointed
+        String name1 = new String("Shivani");
+        String name2 = new String("Shivani");
+        System.out.println(name1 == name2);
+        //System.out.println(name1.equals(name2)); // checks only value
+    }
+}
