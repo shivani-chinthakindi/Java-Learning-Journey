0918d26a15cbcbd1f4dda7a1dae7c737e6f21c75
Shivani Patel <schinthakindishivani@gmail.com>
2026-07-08 19:26:03 +0530

    Complete string declaration

--- 02-Arrays-And-Strings/Strings/StringDeclaration.java
+++ 02-Arrays-And-Strings/Strings/StringDeclaration.java
@@ -1,1 +1,11 @@
-
+class StringDeclaration{
+    public static void main(String[] args) {
+        String name; //Declaration
+        name = "Shivani Patel"; //Intialization
+        System.out.println(name); //Printing output
+        String age = "18"; // Original string with old object
+        System.out.println(age);
+        age = "19"; //just a new object is created but it is not modified
+        System.out.println(age);
+    }
+}
