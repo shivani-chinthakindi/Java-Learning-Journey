4dd5794edc1e39d46f4cef8310f9269f04f812b1
Shivani Patel <schinthakindishivani@gmail.com>
2026-07-12 13:37:38 +0530

    Complete string builder

--- 02-Arrays-And-Strings/Strings/StringBuilderDemo.java
+++ 02-Arrays-And-Strings/Strings/StringBuilderDemo.java
@@ -1,1 +1,12 @@
-
+class StringBuilderDemo {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < 26; i++){
+            char ch = (char)('a'+i);
+            sb.append(ch);
+        }
+        System.out.println(sb.toString());
+        sb.reverse();
+        System.out.println(sb);
+    }
+}
