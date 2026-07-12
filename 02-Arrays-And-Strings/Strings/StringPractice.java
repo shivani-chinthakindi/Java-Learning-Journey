cb6085cfce53da65d09a9cff27b83d024b715bac
Shivani Patel <schinthakindishivani@gmail.com>
2026-07-12 13:59:57 +0530

    Add string practice

--- 02-Arrays-And-Strings/Strings/StringPractice.java
+++ 02-Arrays-And-Strings/Strings/StringPractice.java
@@ -1,1 +1,11 @@
-
+class StringPractice {
+    // pretty printing
+    public static void main(String[] args) {
+        float a = 378.7498f;
+        System.out.printf("Formatted number is %.2f", a);
+        System.out.println();
+        System.out.printf("Pie: %.3f", Math.PI);
+        System.out.println();
+        System.out.printf("Hello my name is %s and I am %s", "Shivani", "Cool");
+    }
+}
