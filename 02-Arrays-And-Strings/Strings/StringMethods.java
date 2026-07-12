5d72eede588541a7730a7e22b5dc00c11bb24c6d
Shivani Patel <schinthakindishivani@gmail.com>
2026-07-12 13:47:56 +0530

    Complete string methods

--- 02-Arrays-And-Strings/Strings/StringMethods.java
+++ 02-Arrays-And-Strings/Strings/StringMethods.java
@@ -1,1 +1,15 @@
+import java.util.Arrays;
 
+class StringMethods {
+    public static void main(String[] args) {
+        String name = "Shivani Patel";
+        System.out.println(name);
+        System.out.println(Arrays.toString(name.toCharArray()));
+        System.out.println(name.toLowerCase());
+        System.out.println(name.indexOf('a'));
+        System.out.println("         Shivani     ".strip());
+        System.out.println(Arrays.toString(name.split(" ")));
+        System.out.println(name.substring(3, 7));
+
+    }
+}
