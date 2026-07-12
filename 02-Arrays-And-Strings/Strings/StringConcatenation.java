31943e78e1b8f94d84f0abd66a7426b513a93ebd
Shivani Patel <schinthakindishivani@gmail.com>
2026-07-12 13:30:36 +0530

    Add string concatenation

--- 02-Arrays-And-Strings/Strings/StringConcatenation.java
+++ 02-Arrays-And-Strings/Strings/StringConcatenation.java
@@ -1,1 +1,14 @@
-
+import java.util.*;
+class StringConcatenation {
+    public static void main(String[] args) {
+        System.out.println('a' + 'b');
+        System.out.println("a" + 'b');
+        System.out.println("a" + "b");
+        System.out.println((char)('a' + 3));
+        System.out.println("a" + 1);
+        System.out.println("Shivani" + new ArrayList<>());
+        System.out.println("Shivani" + Integer.valueOf(56));
+        String ans = Integer.valueOf(56) + " " + new ArrayList<>();
+        System.out.println(ans);
+    }
+}
