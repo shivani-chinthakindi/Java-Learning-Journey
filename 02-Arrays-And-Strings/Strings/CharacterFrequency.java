4291f4f972d44f633003cfac166b7f13861f4d1c
Shivani Patel <schinthakindishivani@gmail.com>
2026-07-19 22:48:17 +0530

    Complete Character Frequency

--- 02-Arrays-And-Strings/Strings/CharacterFrequency.java
+++ 02-Arrays-And-Strings/Strings/CharacterFrequency.java
@@ -1,1 +1,29 @@
-
+class CharacterFrequency {
+    static String modify(String s){
+        String res = " ";
+        int n = s.length();
+        for(int i = 0; i < n; i++){
+            int count = 1;
+            boolean seen = false;
+            for(int j = 0; j < i; j++){
+                if(s.charAt(j) == s.charAt(i)){
+                    seen = true;
+                    break;
+                }
+            }
+            if(seen)
+                continue;
+            for(int j = i+1; j < n; j++){
+                if(s.charAt(j)==s.charAt(i)){
+                    count++;
+                }
+            }
+            res+=s.charAt(i) + Integer.toString(count)+" ";
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        String s = "geeksforgeeks";
+        System.out.println(modify(s));
+    }
+}
