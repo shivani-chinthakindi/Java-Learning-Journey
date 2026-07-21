d4d1de73b5096d2d811f13465fc3a8eb6c574abb
Shivani Patel <schinthakindishivani@gmail.com>
2026-07-21 23:40:20 +0530

    Update string example

--- 02-Arrays-And-Strings/Strings/StringPractice.java
+++ 02-Arrays-And-Strings/Strings/StringPractice.java
@@ -2,7 +2,7 @@ class StringPractice {
     // pretty printing
     public static void main(String[] args) {
         float a = 378.7498f;
-        System.out.printf("Formatted number is %.2f", a);
+        System.out.printf("Formatted number is %.3f", a);
         System.out.println();
         System.out.printf("Pie: %.3f", Math.PI);
         System.out.println();
